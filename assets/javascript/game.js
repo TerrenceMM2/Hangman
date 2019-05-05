66e60faead2de97d1063daf223449d2530a63e92
TerrenceMM2 <terrencemm2@gmail.com>
2019-05-05 16:58:11 -0500

    adds onkeyup and includes methods

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,38 @@
+// GLOBAL VARIABLES
+
 var nashvilleWords = ["Broadway", "Vanderbilt", "Grand Ole Opry", "Ryman", "Parthenon", "Nissan Stadium", "Frist Center", "Cheekwood"];
 var guesses = 15;
 
 // Source: https://www.kirupa.com/html5/picking_random_item_from_array.htm
-var randomWord = nashvilleWords[Math.floor(Math.random() * nashvilleWords.length)];
-
+var randomWord = nashvilleWords[Math.floor(Math.random() * nashvilleWords.length)].toLowerCase();
 console.log(randomWord);
-
-var blankSpace = " _ ";
 // Count without spaces: https://stackoverflow.com/questions/26389745/how-to-count-the-number-of-characters-without-spaces
 var wordLength = randomWord.replace(/\s/g, "").length;
-var nashvilleWord = document.getElementById("nashville-word")
+var nashvilleWord = document.getElementById("nashville-word");
 
-for (i = 0; i < wordLength; i++) {
-    document.getElementById("nashville-word").innerHTML = document.write(blankSpace);
+
+// FUNCTIONS
+
+document.onkeyup = function userGuess(event) {
+    var userGuess = event.key.toLowerCase();
+    console.log(userGuess);
+
+    var searchedLetter = randomWord.includes(userGuess);
+    console.log(searchedLetter);
+
+    // if (searchedLetter === userGuess) {
+    //     console.log("They match");
+    // } else {
+    //     console.log("They don't match");
+    // }
+
 };
 
-// var guessLetters = randomWord.charAt(i);
+
+// LOGIC
+
+for (i = 0; i < wordLength; i++) {
+    document.getElementById("nashville-word").innerHTML = " _ ".repeat(wordLength);
+};
+
+// var guessedLetter = randomWord.search(userGuess);
