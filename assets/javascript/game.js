71adf8ee4245914c509e8eb4a7a98c38549aa451
TerrenceMM2 <terrencemm2@gmail.com>
2019-05-05 21:10:30 -0500

    adds innerHTML for guessed letters and calculated number of allowed guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,23 @@
 // GLOBAL VARIABLES
 
 var nashvilleWords = ["Broadway", "Vanderbilt", "Grand Ole Opry", "Ryman", "Parthenon", "Nissan Stadium", "Frist Center", "Cheekwood"];
-var guesses = 15;
-var guessedLetters = [];
 // Source: https://www.kirupa.com/html5/picking_random_item_from_array.htm
 var randomWord = nashvilleWords[Math.floor(Math.random() * nashvilleWords.length)].toLowerCase();
 console.log(randomWord);
 // Count without spaces: https://stackoverflow.com/questions/26389745/how-to-count-the-number-of-characters-without-spaces
 var wordLength = randomWord.replace(/\s/g, "").length;
 var nashvilleWord = document.getElementById("nashville-word");
+var guesses = guesses(wordLength, 7);
+var guessedLetters = [];
 var userguess;
 
 
 
 // FUNCTIONS
+
+function guesses(x, y) {
+    return x + y;
+}
 
 document.onkeyup = function(event) {
     userGuess = event.key.toLowerCase();
@@ -30,18 +34,20 @@ document.onkeyup = function(event) {
         guesses--;
         console.log(guesses);
         guessedLetters.push(userGuess);
+        document.getElementById("letters-guessed").innerHTML = guessedLetters;
         console.log(guessedLetters);
     } else {
         console.log("They don't match");
         guesses--;
         console.log(guesses);
         guessedLetters.push(userGuess);
+        document.getElementById("letters-guessed").innerHTML = guessedLetters;
         console.log(guessedLetters);
     };
 
     if (guesses === 0) {
         alert("Sorry. You are out of guesses");
-    }
+    };
 };
 
 
@@ -52,3 +58,6 @@ for (i = 0; i < wordLength; i++) {
     document.getElementById("nashville-word").innerHTML = " _ ".repeat(wordLength);
 };
 
+// for (j = 0; j === randomWord.length; j++) {
+//     document.getElementById("letters-guessed").innerHTML = guessedLetters[j] + " ";
+// };
