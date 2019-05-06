5302769500be55ded65d1cd30e36f50a519927ab
TerrenceMM2 <terrencemm2@gmail.com>
2019-05-05 20:49:18 -0500

    adds if/else logic to match user input with random word letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,31 +2,48 @@
 
 var nashvilleWords = ["Broadway", "Vanderbilt", "Grand Ole Opry", "Ryman", "Parthenon", "Nissan Stadium", "Frist Center", "Cheekwood"];
 var guesses = 15;
-
+var guessedLetters = [];
 // Source: https://www.kirupa.com/html5/picking_random_item_from_array.htm
 var randomWord = nashvilleWords[Math.floor(Math.random() * nashvilleWords.length)].toLowerCase();
 console.log(randomWord);
 // Count without spaces: https://stackoverflow.com/questions/26389745/how-to-count-the-number-of-characters-without-spaces
 var wordLength = randomWord.replace(/\s/g, "").length;
 var nashvilleWord = document.getElementById("nashville-word");
+var userguess;
+
 
 
 // FUNCTIONS
 
-document.onkeyup = function userGuess(event) {
-    var userGuess = event.key.toLowerCase();
-    console.log(userGuess);
+document.onkeyup = function(event) {
+    userGuess = event.key.toLowerCase();
+    // console.log(userGuess);
 
-    var searchedLetter = randomWord.includes(userGuess);
-    console.log(searchedLetter);
+    searchedLetter = randomWord.includes(userGuess);
 
-    // if (searchedLetter === userGuess) {
-    //     console.log("They match");
-    // } else {
-    //     console.log("They don't match");
-    // }
+    // Using Regular Expressions to check if the user input was valid (i.e. a letter key only, a-z).
+    // Source: https://stackoverflow.com/questions/2257070/detect-numbers-or-letters-with-jquery-javascript
+    if (!/^[a-z]$/.test(userGuess)) {
+        console.log("This is NOT a valid guess.");
+    } else if (searchedLetter) {
+        console.log("They match");
+        guesses--;
+        console.log(guesses);
+        guessedLetters.push(userGuess);
+        console.log(guessedLetters);
+    } else {
+        console.log("They don't match");
+        guesses--;
+        console.log(guesses);
+        guessedLetters.push(userGuess);
+        console.log(guessedLetters);
+    };
 
+    if (guesses === 0) {
+        alert("Sorry. You are out of guesses");
+    }
 };
+
 
 
 // LOGIC
@@ -35,4 +52,3 @@ for (i = 0; i < wordLength; i++) {
     document.getElementById("nashville-word").innerHTML = " _ ".repeat(wordLength);
 };
 
-// var guessedLetter = randomWord.search(userGuess);
